6570f19c660b832a96bb65ab0253164266301619
NORMss <serega.b301@gmail.com>
2024-06-13 15:30:11 +0700

    feat: DownloadCompletedReceiver added to manifest

--- app/src/main/java/com/norm/aicameraattractions/data/remote/DownloadCompletedReceiver.kt
+++ app/src/main/java/com/norm/aicameraattractions/data/remote/DownloadCompletedReceiver.kt
@@ -4,16 +4,17 @@ import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class DownloadCompletedReceiver : BroadcastReceiver() {
-//    private lateinit var downloadManager: DownloadManager
+    //    private lateinit var downloadManager: DownloadManager
     override fun onReceive(context: Context?, intent: Intent?) {
 //        downloadManager = context?.getSystemService(DownloadManager::class.java)!!
         if (intent?.action == "android.intent.action.DOWNLOAD_COMPLETE") {
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1L)
 //            val query = DownloadManager.Query()
             if (id != -1L) {
-                println("Download with ID $id finished!")
+                Log.d("MyLog", "Download with ID $id finished!")
             }
         }
     }
