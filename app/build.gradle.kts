e89cddcb02d3065ba878c63e6285e2656b04e2bf
NORMss <serega.b301@gmail.com>
2024-08-21 23:10:04 +0700

    feat: updated versions of libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.norm.aicameraattractions"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.norm.aicameraattractions"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
